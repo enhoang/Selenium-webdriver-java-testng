365bfc6e10b6c9945b81390be11c2666a1bea233
En <hoangyen92ltt@gmail.com>
2024-02-28 13:35:30 +0700

    Edit testcase

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -31,6 +31,7 @@ public class Topic_01_Check_Environment {
     }
 
     @Test
+    //edit this line
     public void TC_01_Url() {
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
     }
